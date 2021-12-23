d0341543975debd4160d2075ad69d9ff0365c18f
pragat-pandya <pragat.pandya@gmail.com>
2021-12-23 22:04:49 +0530

    Implement store URL's views. and fix JavaScript loading issue.

--- thekart/settings.py
+++ thekart/settings.py
@@ -109,10 +109,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR/'static'
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
-    'thekart/static',
+    'thekart/static/',
 ]
 
 # Media files config
