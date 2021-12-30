5978d5d50c76079c3aef43df8b44033b7ead4ef1
pragat-pandya <pragat.pandya@gmail.com>
2021-12-30 16:21:51 +0530

    Variations grouping in cart for logged in user across different sessions.

--- accounts/views.py
+++ accounts/views.py
@@ -69,9 +69,33 @@ def login (request):
                 is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
                 if is_cart_item_exists:
                     cart_item = CartItem.objects.filter(cart=cart)
+                    product_variation = []
+                    # GETTING PRODUCT VATIATIONS BY CARTID
                     for item in cart_item:
-                        item.user = user
-                        item.save()
+                        variation = item.variations.all()
+                        product_variation.append(list(variation))
+                    # GET THE CART ITEMS FROM THE USER TO ACCESS HIS PRODUCT VARIATIONS
+                    cart_item = CartItem.objects.filter(user=user)
+                    id = []
+                    ex_var_list = []
+                    for item in cart_item:
+                        exisiting_variation = item.variations.all()
+                        ex_var_list.append(list(exisiting_variation))
+                        id.append(item.id)
+                    # GETING COMMON PRODUCT VATIATION IN product_variation and ex_var_list
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id = id[index]
+                            item = CartItem.objects.get(id=item_id)
+                            item.quantity += 1
+                            item.user = user
+                            item.save()
+                        else:
+                            cart_item = CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
             except:
                 pass
             auth.login(request, user)
