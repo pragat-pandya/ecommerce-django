b8046e8874112244a9649a81e92430520e6fc420
pragat-pandya <pragat.pandya@gmail.com>
2021-12-31 00:34:35 +0530

    Test place_order view with HttpResponse.

--- orders/views.py
+++ orders/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from carts.models import CartItem
+from django.http import HttpResponse
 # Create your views here.
 
 def place_order (request):
-    current_user = request.user
-    cart_items = CartItem.objects.filter(user=current_user)
-    cart_count = cart_items.count()
-    if cart_count < 0:
-        return redirect('store')
+    return HttpResponse('place_order view ')
+    #current_user = request.user
+    #cart_items = CartItem.objects.filter(user=current_user)
+    #cart_count = cart_items.count()
+    #if cart_count < 0:
+#        return redirect('store')
 
-    if request.mehtod == 'POST':
-        form = OrderForm(request.POST)
+#    if request.mehtod == 'POST':
+#        form = OrderForm(request.POST)
