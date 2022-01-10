8eeab3c5269dcce2322b4fe558cbb6d9e03d5fee
pragat-pandya <pragat.pandya@gmail.com>
2022-01-10 15:02:01 +0530

    Move cart items to OrderProduct table.

--- orders/views.py
+++ orders/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from carts.models import CartItem
 from .forms import OrderForm
-from .models import Order, Payment
+from .models import Order, Payment, OrderProduct
 import datetime
 import json
 
@@ -26,6 +26,30 @@ def payments (request):
     order.payment = payment
     order.is_ordered = True
     order.save()
+
+    # Move the cart items to Order Product table
+    cart_item = CartItem.objects.filter(user=request.user)
+
+    for item in cart_item:
+        orderproduct = OrderProduct()
+        orderproduct.order_id = order.id
+        orderproduct.payment = payment
+        orderproduct.user_id = request.user.id
+        orderproduct.product_id = item.product_id
+        orderproduct.quantity = item.quantity
+        orderproduct.product_price = item.product.price
+        orderproduct.ordered = True
+        orderproduct.save()
+
+
+
+    # Reduce the quantity of the sold Products
+
+    # Clear kart
+
+    # Send order recieved email to customer
+
+    # Send order number and transaction id back to sendData method via JSON response
 
     return render (request, "orders/payments.html")
 
