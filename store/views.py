3b8fdcfe16b988586ac3a16b970863fad6565cd2
pragat-pandya <pragat.pandya@gmail.com>
2022-01-11 16:14:29 +0530

    Check if the user has purchased the product before submitting a review for it.

--- store/views.py
+++ store/views.py
@@ -7,6 +7,7 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db.models import Q
 from .forms import ReviewForm
 from django.contrib import messages
+from orders.models import OrderProduct
 # Create your views here.
 
 def store (request, category_slug=None):
@@ -42,9 +43,15 @@ def product_detail (request, category_slug, product_slug):
     except Exception as e:
         raise e
 
+    try:
+        orderproduct = OrderProduct.objects.filter(user=request.user, product_id=single_product.id).exists()
+    except OrderProduct.DoesNotExist:
+        orderproduct = None
+
     context = {
         'single_product' : single_product,
         'in_cart' : in_cart,
+        'orderproduct' : orderproduct,
     }
     return render (request, 'store/product_detail.html', context)
 
