21173ee6e3df87cbf1dc790f054177330e3d8744
pragat-pandya <pragat.pandya@gmail.com>
2022-01-12 18:46:04 +0530

    Edit profile setup.

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
     path('forgotPassword/', views.forgotPassword, name="forgotPassword"),
     path('reset_password_validate/<uidb64>/<token>', views.reset_password_validate, name="reset_password_validate"),
     path('resetPassword/', views.resetPassword, name="resetPassword"),
-    path('my_orders/', views.my_orders, name="my_orders"),    
+    path('my_orders/', views.my_orders, name="my_orders"),
+    path('edit_profile/', views.edit_profile, name="edit_profile"), 
+
 ]
