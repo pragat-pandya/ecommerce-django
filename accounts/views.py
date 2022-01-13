2ac5dcbe6fb0bd86edaac6c2b2f41cc44b3c2c3e
pragat-pandya <pragat.pandya@gmail.com>
2022-01-13 16:17:55 +0530

    Automatically create user profile.

--- accounts/views.py
+++ accounts/views.py
@@ -34,6 +34,12 @@ def register (request):
             user.phone_number = phone_number
             user.save()
 
+            # Create user profile
+            profile = UserProfile()
+            profile.user_id = user.id
+            profile.profile_picture = 'default/default_user.png'
+            profile.save()
+
             # USER ACTIVATION
             current_site = get_current_site(request)
             mail_subject = 'Please activate your account.'
