c67b0c63df73a1afc5d1c3d71d58609aa0ead968
pragat-pandya <pragat.pandya@gmail.com>
2022-01-13 16:52:12 +0530

    Change Password from dashboard.

--- accounts/urls.py
+++ accounts/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('reset_password_validate/<uidb64>/<token>', views.reset_password_validate, name="reset_password_validate"),
     path('resetPassword/', views.resetPassword, name="resetPassword"),
     path('my_orders/', views.my_orders, name="my_orders"),
-    path('edit_profile/', views.edit_profile, name="edit_profile"), 
-
+    path('edit_profile/', views.edit_profile, name="edit_profile"),
+    path('change_password/', views.change_password, name="change_password"),
 ]
